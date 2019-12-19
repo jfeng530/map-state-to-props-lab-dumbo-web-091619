92627165a86eae60ae98ec597081e40e34526c1a
Jacky Feng <jackyfeng530@yahoo.com>
2019-12-19 16:22:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import manageUsers from './reducers/manageUsers'
 
 import manageUsers from './reducers/manageUsers'
 
