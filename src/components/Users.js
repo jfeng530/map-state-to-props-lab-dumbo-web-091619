bb25169612b0a6683e08f432928cf3f83f760d73
Jacky Feng <jackyfeng530@yahoo.com>
2019-12-19 16:26:32 +0000

    Automatically backed up by Learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -8,6 +8,7 @@ class Users extends Component {
         <ul>
           Users!
           {/* Write code here that displays the usernames of all users in the Redux store */}
+          
           {/* In addition, display the total number of users curently in the store */}
         </ul>
       </div>
